6a2983306265c108c0c9cc01199587f66c560236
celine <89533499+celineai@users.noreply.github.com>
2025-05-06 18:05:49 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("====================")
+print("Area Calculator")
+print("====================")
+
+print("1) Triangle")
+print("2) Rectangle")
+print("3) Square")
+print("4) Circle")
+print("5) Quit")
